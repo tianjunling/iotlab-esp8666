93e79c62ec4f8142fb60a0c5209487313132b93e
BKinya <gitongakinya1@gmail.com>
2018-09-14 16:42:38 +0300

    initial commit

---
+++ esp8266-dht11.ino
@@ -0,0 +1,356 @@
+/**
+ IBM IoT Foundation managed Device
+
+ Author: Ant Elder
+ License: Apache License v2
+*/
+//libraries
+#include <TimeLib.h>
+#include <DHT.h>;
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
+#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson/releases/tag/v5.0.7
+#include <WiFiUdp.h>
+
+ 
+
+//-------- Customise these values -----------
+const char* ssid = "beatrice";
+const char* password = "beatrice";
+
+//Constants
+#define DHTPIN 4     // what pin we're connected to
+#define DHTTYPE DHT11   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+#define ORG "22tdf0"
+#define DEVICE_TYPE "ESP8266"
+#define DEVICE_ID "port1"
+#define TOKEN "4_CvlB8?MdF-hWdX0)"
+//-------- Customise the above values --------
+
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+const char publishTopic[] = "iot-2/evt/status/fmt/json";
+const char responseTopic[] = "iotdm-1/response";
+const char manageTopic[] = "iotdevice-1/mgmt/manage";
+const char updateTopic[] = "iotdm-1/device/update";
+const char rebootTopic[] = "iotdm-1/mgmt/initiate/device/reboot";
+
+// NTP Servers:
+IPAddress timeServer(176, 9, 102, 215); // time-a.timefreq.bldrdoc.gov
+
+const int timeZone = 3; //central european time
+
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+//udp
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+
+void callback(char* topic, byte* payload, unsigned int payloadLength);
+
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, callback, wifiClient);
+
+int publishInterval = 30000; // 30 seconds
+long lastPublishMillis;
+
+void setup() {
+ Serial.begin(115200); 
+ Serial.println();
+ 
+ wifiConnect();
+ udpConnect();
+ mqttConnect();
+ initManagedDevice();
+
+  
+}
+
+
+time_t prevDisplay = 0;
+void loop() {
+  if (timeStatus() != timeNotSet) {
+    if (now() != prevDisplay) { //update the display only if time has changed
+      prevDisplay = now();
+      
+     if (millis() - lastPublishMillis > publishInterval) {
+     getAndSendTemperatureAndHumidityData();
+     lastPublishMillis = millis();
+ }
+
+    }
+  }
+
+  delay(1000);
+ 
+ if (!client.loop()) {
+   mqttConnect();
+   initManagedDevice();
+ }
+}
+
+void wifiConnect() {
+ Serial.print("Connecting to "); Serial.print(ssid);
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   Serial.print(".w");
+ }
+ Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+}
+
+void udpConnect(){
+  Serial.print("IP number assigned by DHCP is ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+  Serial.println("waiting for sync");
+  setSyncProvider(getNtpTime);
+}
+void mqttConnect() {
+ if (!client.connected()) {
+   Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+   while (!!!client.connect(clientId, authMethod, token)) {
+     Serial.print(".m");
+     delay(500);
+   }
+   Serial.println();
+ }
+}
+
+void initManagedDevice() {
+ if (client.subscribe("iotdm-1/response")) {
+   Serial.println("subscribe to responses OK");
+ } else {
+   Serial.println("subscribe to responses FAILED");
+ }
+
+ if (client.subscribe(rebootTopic)) {
+   Serial.println("subscribe to reboot OK");
+ } else {
+   Serial.println("subscribe to reboot FAILED");
+ }
+
+ if (client.subscribe("iotdm-1/device/update")) {
+   Serial.println("subscribe to update OK");
+ } else {
+   Serial.println("subscribe to update FAILED");
+ }
+
+ StaticJsonBuffer<300> jsonBuffer;
+ JsonObject& root = jsonBuffer.createObject();
+ JsonObject& d = root.createNestedObject("d");
+ JsonObject& metadata = d.createNestedObject("metadata");
+ metadata["publishInterval"] = publishInterval;
+ JsonObject& supports = d.createNestedObject("supports");
+ supports["deviceActions"] = true;
+
+ char buff[300];
+ root.printTo(buff, sizeof(buff));
+ Serial.println("publishing device metadata:"); 
+ Serial.println(buff);
+ if (client.publish(manageTopic, buff)) {
+   Serial.println("device Publish ok");
+ } else {
+   Serial.print("device Publish failed:");
+ }
+}
+
+
+void getAndSendTemperatureAndHumidityData()
+{
+  Serial.println("Collecting temperature data.");
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+
+  String temperature = String(t);
+  String humidity = String(h);
+  //time and date
+  String ho = String(hour());
+  String mi = String(minute());
+  String se = String(second());
+  String da = String(day()); 
+  String mo = String(month());
+  String ye = String(year());
+
+  // Prepare a JSON payload string
+  String payload = "{";
+  payload += "\" device_id\":";
+  payload += DEVICE_ID; payload +=",";
+  payload += "\"temperature\":"; 
+  payload += temperature; payload += ",";
+  payload += "\"humidity\":"; 
+  payload += humidity; payload += ",";
+  payload += "\" Time\":";
+  payload += ho; payload += ".";
+  payload += mi; payload += ".";
+  payload += se; payload += ",";
+  payload += "\" day\":"; 
+  payload += da; payload += ".";
+  payload += mo; payload += ".";
+  payload += ye; 
+  payload += "}";
+
+
+Serial.print("Sending payload: "); Serial.println(payload);
+
+ if (client.publish(publishTopic, (char*) payload.c_str())) {
+   Serial.println("Publish OK");
+ } else {
+   Serial.println("Publish FAILED");
+ }
+  
+}
+/**--------------------------------------------------------------------------------------
+                                  Timestamp 
+--------------------------------------------------------------------------------------*/
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(".");
+  Serial.print(month());
+  Serial.print(".");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+
+
+void printDigits(int digits){
+  // utility for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  sendNTPpacket(timeServer);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:                 
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+/**-------------------------------------------------------------------------------------
+                                  End
+ --------------------------------------------------------------------------------------*/
+
+
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+ Serial.print("callback invoked for topic: "); Serial.println(topic);
+
+ if (strcmp (responseTopic, topic) == 0) {
+   return; // just print of response for now
+ }
+
+ if (strcmp (rebootTopic, topic) == 0) {
+   Serial.println("Rebooting...");
+   ESP.restart();
+ }
+
+ if (strcmp (updateTopic, topic) == 0) {
+   handleUpdate(payload);
+ }
+}
+
+void handleUpdate(byte* payload) {
+ StaticJsonBuffer<300> jsonBuffer;
+ JsonObject& root = jsonBuffer.parseObject((char*)payload);
+ if (!root.success()) {
+   Serial.println("handleUpdate: payload parse FAILED");
+   return;
+ }
+ Serial.println("handleUpdate payload:"); root.prettyPrintTo(Serial); Serial.println();
+
+ JsonObject& d = root["d"];
+ JsonArray& fields = d["fields"];
+ for (JsonArray::iterator it = fields.begin(); it != fields.end(); ++it) {
+   JsonObject& field = *it;
+   const char* fieldName = field["field"];
+   if (strcmp (fieldName, "metadata") == 0) {
+     JsonObject& fieldValue = field["value"];
+     if (fieldValue.containsKey("publishInterval")) {
+       publishInterval = fieldValue["publishInterval"];
+       Serial.print("publishInterval:"); Serial.println(publishInterval);
+     }
+   }
+ }
+}
